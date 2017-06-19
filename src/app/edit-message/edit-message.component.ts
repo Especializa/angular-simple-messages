6510b6d5f87682bed0e8fcfbbc622cbd24abf602
Jose Berardo Cunha <joseberardo@gmail.com>
2017-06-20 01:30:41 +1000

    route params start

---
+++ src/app/edit-message/edit-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-message',
+  templateUrl: './edit-message.component.html',
+  styleUrls: ['./edit-message.component.scss']
+})
+export class EditMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/edit-message/edit-message.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditMessageComponent } from './edit-message.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EditMessageComponent]
+})
+export class EditMessageModule { }
